eb14bb4ca699a0d12ea6c604312f2040e3995ef1
Momo Amer <maea@it-kartellet.dk>
2025-04-08 21:30:20 +0200

    Initial commit

---
+++ BlazorApp1/Pages/Cpr.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BlazorApp1;
+
+public class Cpr : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
---
+++ BlazorApp1/Pages/TodoList.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BlazorApp1.Pages;
+
+public class TodoList : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
